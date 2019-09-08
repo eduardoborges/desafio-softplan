618aca341b6765921cec797aab57d1d16201ae0b
Eduardo Borges <euduardoborges@gmail.com>
2019-09-08 16:00:25 -0300

    (todo): characters screen connected to redux

--- src/screens/characters/index.tsx
+++ src/screens/characters/index.tsx
@@ -1,7 +1,13 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { RouteComponentProps, Link } from '@reach/router';
+import { AppState } from 'store';
+import { indexRequest } from 'store/characters/actions';
+import debouce from 'lodash.debounce';
 
 
 interface OwnProps {
@@ -10,77 +16,92 @@ interface OwnProps {
 
 type Props = OwnProps & RouteComponentProps;
 
-const CharactersScreen: React.FunctionComponent<Props> = props => (
-  <div className="section">
-    <div className="container">
+const CharactersScreen: React.FunctionComponent<Props> = (props) => {
+  const characters = useSelector((state:AppState) => state.CHARACTERS);
+  const dispatch = useDispatch();
 
-      <div className="columns">
-        <div className="column">
 
-          <h1 className="title">Personagens</h1>
-        </div>
+  const isEndScroll = () => {
+    const position = window.innerHeight + document.documentElement.scrollTop;
+    const height = document.documentElement.offsetHeight;
+    return position + 150 >= height;
+  };
 
-        <div className="column">
-          <div className="field has-addons">
-            <div className="control is-expanded">
-              <input className="input is-medium" type="text" placeholder="Encontre um personagem" />
-            </div>
-            <div className="control">
-              <a className="button is-medium">
-                Search
-              </a>
-            </div>
+  useEffect(() => {
+    dispatch(indexRequest());
+  }, [dispatch]);
+
+  return (
+    <div className="section">
+      <div className="container">
+        <div className="columns">
+
+          <div className="column">
+            <h1 className="title">
+              Personagens
+              {' '}
+              {characters.loading && <small>...</small>}
+            </h1>
+
           </div>
-        </div>
 
-      </div>
-
-      <div className="columns is-multiline">
-        {Array.from(new Array(25)).map((v, i) => (
-          <div className="column is-4">
-            <Link to="/characters/1">
-
-              <div className="card">
-                <div className="card-image">
-                  <figure className="image is-4by3">
-                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
-                  </figure>
-                </div>
-                <div className="card-content">
-                  <div className="media">
-                    <div className="media-left">
-                      <figure className="image is-48x48">
-                        <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
-                      </figure>
-                    </div>
-                    <div className="media-content">
-                      <p className="title is-4">John Smith</p>
-                      <p className="subtitle is-6">@johnsmith</p>
-                    </div>
-                  </div>
-
-                  <div className="content">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Phasellus nec iaculis mauris.
-                    {' '}
-                    <a>@bulmaio</a>
-                  .
-                    <a href="#">#css</a>
-                    {' '}
-                    <a href="#">#responsive</a>
-                    <br />
-                    <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-                  </div>
-                </div>
+          <div className="column">
+            <div className="field has-addons">
+              <div className="control is-expanded">
+                <input className="input is-medium" type="text" placeholder="Encontre um personagem" />
               </div>
-            </Link>
+              <div className="control">
+                <a className="button is-medium">
+                  Search
+                </a>
+              </div>
+            </div>
           </div>
-        ))}
+
+        </div>
+
+        <div className="columns is-multiline">
+          {characters.data.map(c => (
+            <div className="column is-4" key={c.name}>
+              <Link to="/characters/1">
+                <div className="card">
+                  <div className="card-image">
+                    <figure className="image is-4by3">
+                      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                    </figure>
+                  </div>
+                  <div className="card-content">
+                    <div className="media">
+                      <div className="media-left">
+                        <figure className="image is-48x48">
+                          <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+                        </figure>
+                      </div>
+                      <div className="media-content">
+                        <p className="title is-4">{c.name}</p>
+                        <p className="subtitle is-6">@johnsmith</p>
+                      </div>
+                    </div>
+
+                    <div className="content" />
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+
+        <div className="columns">
+          <div className="column">
+            <button type="button" className="button is-fullwidth">Carregar mais</button>
+            <div className="title has-text-centered has-text-grey">{characters.loading ? 'Carregando...' : '•'}</div>
+          </div>
+        </div>
+
+
       </div>
-
-
     </div>
-  </div>
-);
+  );
+};
 
 export default CharactersScreen;
