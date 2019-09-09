7fb8afad223c3a22f92d21c5042d5e76f8c7a3c2
Eduardo Borges <euduardoborges@gmail.com>
2019-09-09 10:49:48 -0300

    (test): saga test mock

---
+++ src/store/characters/sagas.test.ts
@@ -0,0 +1,11 @@
+import { call, put, select } from 'redux-saga/effects';
+
+import { } from './actions';
+import { } from './types';
+
+
+describe('Characters Sagas Test', () => {
+  test('Test get characters', () => {
+    expect(true).toBeTruthy();
+  });
+});
